a6560b7869618a3a12d532e2c70ec51049973064
Sahajbeer Singh <sahajbeer315@gmail.com>
2026-04-28 13:29:07 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+load_dotenv()
+import subprocess
+import requests
+import os
+
+app = Flask(__name__)
+
+@app.route("/run", methods=["POST"])
+def run_code():
+    code = request.json["code"]
+
+    with open("input.txt", "w", encoding="utf-8") as f:
+        f.write(code)
+
+    result = subprocess.run(
+        ["a.exe", "input.txt"],
+        capture_output=True,
+        text=True,
+        encoding="utf-8",
+        errors="ignore"
+    )
+
+    return jsonify({
+        "output": result.stdout,
+        "error": result.stderr
+    })
+api_key = os.getenv("GROQ_API_KEY")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    try:
+        user_msg = request.json["message"]
+
+        response = requests.post(
+            "https://api.groq.com/openai/v1/chat/completions",
+            headers={
+                "Authorization": "Bearer " + api_key,
+                "Content-Type": "application/json"
+            },
+            json={
+                "model": "openai/gpt-oss-20b",
+                "messages": [
+                    {"role": "user", "content": user_msg}
+                ]
+            }
+        )
+
+        data = response.json()
+
+        if "choices" in data:
+            reply = data["choices"][0]["message"]["content"]
+        elif "error" in data:
+            reply = "API Error: " + data["error"]["message"]
+        else:
+            reply = "Unknown error"
+
+        return jsonify({"reply": reply})
+
+    except Exception as e:
+        print("ERROR:", e) 
+        return jsonify({"reply": "Server error"}), 500
+
+@app.route("/")
+def home():
+    return open("index.html", encoding="utf-8").read()
+
+app.run(debug=True)
