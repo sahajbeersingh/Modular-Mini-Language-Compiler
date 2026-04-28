205a3c04cdf3baf8001c9ca11c0b12a5266542eb
Sahajbeer Singh <sahajbeer315@gmail.com>
2026-04-28 12:40:17 +0530

    Add new token types and reorganize existing ones

--- token.h
+++ token.h
@@ -5,38 +5,39 @@
 #include <iostream>
 
 enum TokenType {
-    // keywords
-    TOKEN_INT,
-    TOKEN_IF,
-    TOKEN_ELSE,
-    TOKEN_PRINT,
+    TOKEN_VAR,        
+    TOKEN_CHECK,      
+    TOKEN_OTHERWISE,  
+    TOKEN_SHOW,       
+    TOKEN_WHILE,      
+    TOKEN_FOR,        
     
-    // identifiers & literals
+  
     TOKEN_IDENTIFIER,
     TOKEN_NUMBER,
     
-    // operators
-    TOKEN_ASSIGN,    // =
-    TOKEN_PLUS,      // +
-    TOKEN_MINUS,     // -
-    TOKEN_MULTIPLY,  // *
-    TOKEN_DIVIDE,    // /
-    TOKEN_EQUAL,     // ==
-    TOKEN_NOT_EQUAL, // !=
-    TOKEN_LESS,      // <
-    TOKEN_GREATER,   // >
-    TOKEN_LESS_EQUAL,    // <=
-    TOKEN_GREATER_EQUAL, // >=
     
-    // delimiters
-    TOKEN_SEMICOLON, // ;
-    TOKEN_COMMA,     // ,
-    TOKEN_LPAREN,    // (
-    TOKEN_RPAREN,    // )
-    TOKEN_LBRACE,    // {
-    TOKEN_RBRACE,    // }
+    TOKEN_ASSIGN,        
+    TOKEN_PLUS,          
+    TOKEN_MINUS,         
+    TOKEN_MULTIPLY,      
+    TOKEN_DIVIDE,        
+    TOKEN_EQUAL,         
+    TOKEN_NOT_EQUAL,     
+    TOKEN_LESS,          
+    TOKEN_GREATER,       
+    TOKEN_LESS_EQUAL,    
+    TOKEN_GREATER_EQUAL, 
     
-    // special
+   
+    TOKEN_SEMICOLON, 
+    TOKEN_COMMA,     
+    TOKEN_LPAREN,    
+    TOKEN_RPAREN,    
+    TOKEN_LBRACE,    
+    TOKEN_RBRACE,    
+    
+    
     TOKEN_EOF,
     TOKEN_ERROR
 };
@@ -48,16 +49,18 @@ public:
     int line;
     int col;
     
-    Token(TokenType t, std::string v, int l, int c) 
+    Token(TokenType t, std::string v, int l, int c)
         : type(t), value(v), line(l), col(c) {}
     
     std::string toString() {
         std::string typeStr;
         switch(type) {
-            case TOKEN_INT: typeStr = "INT"; break;
-            case TOKEN_IF: typeStr = "IF"; break;
-            case TOKEN_ELSE: typeStr = "ELSE"; break;
-            case TOKEN_PRINT: typeStr = "PRINT"; break;
+            case TOKEN_VAR: typeStr = "VAR"; break;
+            case TOKEN_CHECK: typeStr = "CHECK"; break;
+            case TOKEN_OTHERWISE: typeStr = "OTHERWISE"; break;
+            case TOKEN_SHOW: typeStr = "SHOW"; break;
+            case TOKEN_WHILE: typeStr = "WHILE"; break;
+            case TOKEN_FOR: typeStr = "FOR"; break;
             case TOKEN_IDENTIFIER: typeStr = "IDENTIFIER"; break;
             case TOKEN_NUMBER: typeStr = "NUMBER"; break;
             case TOKEN_ASSIGN: typeStr = "ASSIGN"; break;
